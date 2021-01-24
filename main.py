095017da0a7f0809e85204d7f7fe3331684c3161
Seunghoon Shin <liante0904@gmail.com>
2021-01-24 13:18:01 +0900

    Add emoji, Clearing code, Prepare to struct more SEC

--- main.py
+++ main.py
@@ -19,42 +19,21 @@ from bs4 import BeautifulSoup
 
 # 기본 URL
 ARTICLE_BASE_URL = ""
-# 파싱 게시판 개수
-BOARD_CNT = 3 
 # 게시판 이름
 BOARD_NAME  = ["이슈브리프" , "기업분석", "산업분석"]
-
+# 게시글 갱신 시간
+REFRESH_TIME = 600
 # 텔레그램 발송 메세지 변수
 sendMessageText = ""
 # 발송한 연속키
-nNxtIdx = [0, 0, 3851]
+#nNxtIdx = [2279, 7617, 3848]
+nNxtIdx = [0, 0, 0]
 # 새로 올라온 게시글 개수
 nNewFeedCnt = 0
 
-def send():
-
-    global sendMessageText
-    
-    #생성한 텔레그램 봇 정보 assign (@ebest_noti_bot)
-    my_token_key = '1372612160:AAHVyndGDmb1N2yEgvlZ_DmUgShqk2F0d4w'
-    
-    bot = telegram.Bot(token = my_token_key)
-
-    #생성한 텔레그램 봇 정보 출력
-    me = bot.getMe()
-    #print('텔레그램 채널 정보 : ',me)
-
-    #생성한 텔레그램 봇 /start 시작 후 사용자 id 받아 오기
-    #chat_id = bot.getUpdates()[-1].message.chat.id
-
-    chat_id = '-1001431056975'
-    
-
-    #print('user id : ', chat_id)
-
-    bot.sendMessage(chat_id=chat_id, text = sendMessageText)
-    time.sleep(10) # 모바일 알림을 받기 위해 10초 텀을 둠(loop 호출시)
-
+# 이모지
+fire = u'\U0001F525'
+pick = u'\U0001F449'
 
 def checkNewArticle():
 
@@ -66,9 +45,6 @@ def checkNewArticle():
     # 1. 앞에 "https://www.ebestsec.co.kr/_bt_lib/util/download.jsp?dataType=" 를 추가
     # 2. 링크에서 알맹이를 붙이면 됨 -> javascript:download("08573D2F59307A57F4FC67A81B8C333A4C884E6D2951A32F4A48B73EF4E6EC22A0E62B351A025A54E20CB47DEF8A0A801BF2F7B5E3E640975E88D7BACE3B4A49F83020ED90019B489B3C036CF8AB930DCF4795CE87DE76454465F0CF7316F47BF3A0BC08364132247378E3AABC8D0981627BD8F94134BF00D27B03D8F04AC8C04369354956052B75415A9585589694B5F63378DFA40C6BA6435302B96D780C3B3EB2BF0C866966D4CE651747574C8B25208B848CBEBB1BE0222821FC75DCE016")
 
-    global sendMessageText
-    global nNxtIdx
-    global nNewFeedCnt
     global ARTICLE_BASE_URL
 
     requests.packages.urllib3.disable_warnings()
@@ -87,19 +63,12 @@ def checkNewArticle():
     for idx, TARGET_URL in enumerate(TARGET_URL_TUPLE):
         parse(idx, TARGET_URL)
         time.sleep(5)
-        
+ 
 
 def parse(idx, TARGET_URL):
-
-    # 파싱 게시판 개수
-    global BOARD_CNT 
-    # 게시판 이름
-    global BOARD_NAME
-
     global sendMessageText
     global nNxtIdx
     global nNewFeedCnt
-    global ARTICLE_BASE_URL
 
     webpage = requests.get(TARGET_URL, verify=False)
 
@@ -111,32 +80,34 @@ def parse(idx, TARGET_URL):
     #nNxtIdx[idx] = int( totalIdx[0].text.replace("Total", "").replace("Page 1", "").strip() )
     print('전체 게시글', ntotalIdx, '게시글 연속키', nNxtIdx[idx])
     if nNxtIdx[idx] == 0: # 첫 실행인 경우 임의로 가장 마지막 게시글을 발송
+        print('###첫실행구간###')
         # 게시글 제목
         soup = soup.find_all('td', class_='subject')
-        #top_200_list = soup.find_all('li', class_='chart-list__element')
-        #soup = str(soup).replace("amp;", "")
-        #print(soup)
 
         print('게시판 이름:', BOARD_NAME[idx]) # 게시판 종류
         print('게시글 제목:',soup[0].find('a').text ) # 게시글 제목
-        
-        articleTitle = soup[0].find('a').text + "\n"
+        print('게시글URL:',ARTICLE_BASE_URL + soup[0].find('a').attrs['href'].replace("amp;", "")) # 주소
+        print('############')
+
+        articleTitle = ''
+        articleTitle += fire+ BOARD_NAME[idx] + fire + "\n"
+        articleTitle += soup[0].find('a').text + "\n"
         sendMessageText = articleTitle 
-        sendMessageText += ARTICLE_BASE_URL 
+        sendMessageText += pick + ARTICLE_BASE_URL 
         sendMessageText += soup[0].find('a').attrs['href'].replace("amp;", "")
         
-        print(ARTICLE_BASE_URL + soup[0].find('a').attrs['href'].replace("amp;", "")) # 주소
         #send() # 서버 재 실행시 첫 발송 주석
         nNxtIdx[idx] = ntotalIdx # 첫 실행시 인덱스 설정
 
     else: # 두번째 실행인 경우
-        print('게시판 이름:', BOARD_NAME[idx]) # 게시판 종류
+        print('###ELSE구간###')
         soup = soup.find_all('td', class_='subject')
         #nIdx = int(soup.select('tbody > tr > td')[0]) # 현재 게시글 번호
         nNewFeedCnt = ntotalIdx - nNxtIdx[idx] # 새로 올라온 게시글 개수
         
+        print('### 새로운 게시글 개수:', nNewFeedCnt,' ###')
         while nNewFeedCnt > 0: # 새 게시글이 올라옴
-            print('새로운 게시글 수:', nNewFeedCnt)
+            print('현재 게시판 :',BOARD_NAME[idx],' 새로운 게시글 수:', nNewFeedCnt)
             articleTitle = soup[nNewFeedCnt-1].find('a').text + "\n"
             sendMessageText = articleTitle 
             sendMessageText += ARTICLE_BASE_URL 
@@ -145,12 +116,33 @@ def parse(idx, TARGET_URL):
             nNewFeedCnt -= 1
             print('nNewFeedCnt', nNewFeedCnt)
             if nNewFeedCnt == 0 : 
+                print('새로운 게시글 모두 전송 완료')
                 nNxtIdx[idx] = ntotalIdx
                 return
 
-
         return False
 
+def send():
+
+    global sendMessageText
+    
+    #생성한 텔레그램 봇 정보 assign (@ebest_noti_bot)
+    my_token_key = '1372612160:AAHVyndGDmb1N2yEgvlZ_DmUgShqk2F0d4w'
+    bot = telegram.Bot(token = my_token_key)
+
+    #생성한 텔레그램 봇 정보 출력
+    #me = bot.getMe()
+    #print('텔레그램 채널 정보 : ',me)
+
+    #생성한 텔레그램 봇 /start 시작 후 사용자 id 받아 오기
+    #chat_id = bot.getUpdates()[-1].message.chat.id
+
+    #chat_id = '-1001431056975' # 이베스트 게시물 알림 채널
+    chat_id = '-1001474652718' # 테스트 채널
+
+    bot.sendMessage(chat_id = chat_id, text = sendMessageText)
+    time.sleep(8) # 모바일 알림을 받기 위해 8초 텀을 둠(loop 호출시)
+       
 
 
 # 액션 플랜 
@@ -159,14 +151,12 @@ def parse(idx, TARGET_URL):
     # 메세지로 게시글 정보르 보냅니다
     # 아닌 경우 다시 1번을 반복합니다.
 def main():
+    print('########Program Start Run########')
     while True:
         print("checkNewArticle() => 새 게시글 정보 확인")
-        if checkNewArticle():
-            print("send() => 게시글 정보 보내기")
-            #send()
-
-        time.sleep(600)
-        print("게시글 재확인")
+        checkNewArticle()
+        time.sleep(REFRESH_TIME)
+        print('######',REFRESH_TIME,'초 후 게시글 재 확인######')
 
 
 if __name__ == "__main__":
