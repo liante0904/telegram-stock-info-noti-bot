853238eead3afe9afab1646449847c2d81dfe467
seunghoon-shin <liante0904@gmail.com>
2021-02-03 09:56:51 +0900

    Add HanaSec Daily Board. inti

--- main.py
+++ main.py
@@ -505,8 +505,8 @@ def HANA_parse(ARTICLE_BOARD_ORDER, TARGET_URL):
         LIST_ATTACT_FILE_NAME = list.select_one('div.con > ul > li:nth-child(5) > div > a').text
 
         if NXT_KEY != LIST_ARTICLE_TITLE or NXT_KEY == '': #  
-            HANA_downloadFile(LIST_ARTICLE_URL, LIST_ATTACT_FILE_NAME)
-            send(ARTICLE_BOARD_NAME = ARTICLE_BOARD_NAME, ARTICLE_TITLE = LIST_ARTICLE_TITLE, ARTICLE_URL = LIST_ARTICLE_URL)
+            # HANA_downloadFile(LIST_ARTICLE_URL, LIST_ATTACT_FILE_NAME)
+            # send(ARTICLE_BOARD_NAME = ARTICLE_BOARD_NAME, ARTICLE_TITLE = LIST_ARTICLE_TITLE, ARTICLE_URL = LIST_ARTICLE_URL)
             print('메세지 전송 URL:', LIST_ARTICLE_URL)
         else:
             print('새로운 게시물을 모두 발송하였습니다.')
