2b250ed24a98ccf8c3260b322e089ccc72a838a4
Seunghoon Shin <liante0904@gmail.com>
2023-07-31 21:09:31 +0900

    Edit gd.

--- googledrive.py
+++ googledrive.py
@@ -3,7 +3,7 @@ from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 
-def gd(*args):
+def upload(*args):
     print('********************')
     strFileName = ''
 
