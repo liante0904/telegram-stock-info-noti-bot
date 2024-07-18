4dd2a4136d2a8ac19a88fce7a2827fc3fb7fe9ba
seunghoon shin <liante0904@gmail.com>
2024-07-18 11:27:31 +0000

    Sync changed files from master to test

--- news.py
+++ news.py
@@ -2,25 +2,17 @@
 import os
 import telegram
 import requests
-import datetime
 import logging
 import time
 import json
-import re
 import asyncio
-import pymysql
 import pymysql.cursors
-import urllib.parse as urlparse
 import urllib.request
 
-from package import googledrive
 from package.common import *
-from package.firm_info import *
 from package.json_util import save_data_to_local_json  # import the function from json_util
 
 from package.SecretKey import SecretKey
-from package.MariaDB import MariaDB
-from requests import get  # to make GET request
 
 # 로직 설명
 # 1. Main()-> 각 회사별 함수를 통해 반복 (추후 함수명 일괄 변경 예정)
@@ -34,32 +26,14 @@ from requests import get  # to make GET request
 # 5. 메시지 발송 방법 변경 (봇 to 사용자 -> 채널에 발송)
 
 ############공용 상수############
-# sleep key
-SLEEP_KEY_DIR_FILE_NAME = './key/sleep.key'
-INTERVAL_TIME = 3 # 10분 단위 적용
-INTERVAL_INIT_TIME = 1
-
-# 게시글 갱신 시간
-REFRESH_TIME = 60 * 20 # 20분
 
 # pymysql 변수
 conn    = ''
 cursor  = ''
 
-# 연속키URL
-NXT_KEY = ''
-NXT_KEY_ARTICLE_TITLE = ''
 # 게시판 URL
 BOARD_URL = ''
-# 테스트 발송여부
-TEST_SEND_YN = ''
-# 텔레그램 채널 발송 여부
-SEND_YN = ''
-TODAY_SEND_YN = ''
-# 텔레그램 마지막 메세지 발송시간(단위 초)
-SEND_TIME_TERM = 0 # XX초 전에 해당 증권사 메시지 발송
 # 첫번째URL 
-FIRST_ARTICLE_URL = ''
 # SendAddText 글로벌 변수
 SEND_ADD_MESSAGE_TEXT = ''
 # LOOP 인덱스 변수
@@ -84,12 +58,6 @@ BOARD_NM = ''
 SECRET_KEY = SecretKey()
 SECRET_KEY.load_secrets()
 
-# 데이터베이스 연결 초기화
-db_url = 'your_database_url_here'
-db = MariaDB(SECRET_KEY.ORACLECLOUD_MYSQL_DATABASE_URL)
-db.open_connect()
-
-
 def ChosunBizBot_checkNewArticle():
     global ARTICLE_BOARD_ORDER
     global SEC_FIRM_ORDER
@@ -111,8 +79,6 @@ def ChosunBizBot_checkNewArticle():
  
 # 증권플러스 뉴스 JSON API 타입
 def ChosunBizBot_StockPlusJSONparse(ARTICLE_BOARD_ORDER, TARGET_URL):
-    global NXT_KEY
-    global TEST_SEND_YN
     request = urllib.request.Request(TARGET_URL, headers={'User-Agent': 'Mozilla/5.0'})
     response = urllib.request.urlopen(request)
     rescode = response.getcode()
@@ -124,35 +90,6 @@ def ChosunBizBot_StockPlusJSONparse(ARTICLE_BOARD_ORDER, TARGET_URL):
         return True
 
     jres = jres['newsItems']
-
-    try:
-        FIRST_ARTICLE_URL = jres[0]['url']
-        FIRST_ARTICLE_TITLE = jres[0]['title']
-    except:
-        FIRST_ARTICLE_URL = ''
-        FIRST_ARTICLE_TITLE = ''
-            
-    print('FIRST_ARTICLE_TITLE:',FIRST_ARTICLE_TITLE)
-    
-    # # 연속키 데이터베이스화 작업
-    # # 연속키 데이터 저장 여부 확인 구간
-    # dbResult = DB_SelNxtKey(SEC_FIRM_ORDER, ARTICLE_BOARD_ORDER)
-    # if dbResult: # 1
-    #     # 연속키가 존재하는 경우
-    #     print('데이터베이스에 연속키가 존재합니다. ','(ChosunBizBot_JSONparse)')
-
-    # else: # 0
-    #     # 연속키가 존재하지 않는 경우 => 첫번째 게시물 연속키 정보 데이터 베이스 저장
-    #     print('데이터베이스에 ', '(ChosunBizBot_JSONparse)')
-    #     NXT_KEY = DB_InsNxtKey(SEC_FIRM_ORDER, ARTICLE_BOARD_ORDER, FIRST_ARTICLE_TITLE)
-
-
-    # # 연속키 체크
-    # r = isNxtKey(FIRST_ARTICLE_TITLE)
-    # if SEND_YN == 'Y' : r = ''
-    # if r: 
-    #     print('*****최신 게시글이 채널에 발송 되어 있습니다. 연속키 == 첫 게시물****')
-    #     return ''
     
 
     nNewArticleCnt = 0
@@ -210,8 +147,6 @@ def NAVERNews_checkNewArticle_0():
 
 # JSON API 타입
 def NAVERNews_parse_0(ARTICLE_BOARD_ORDER, TARGET_URL):
-    global NXT_KEY
-    global TEST_SEND_YN
 
     print('NAVERNews_parse_0')
     request = urllib.request.Request(TARGET_URL, headers={'User-Agent': 'Mozilla/5.0'})
@@ -226,23 +161,6 @@ def NAVERNews_parse_0(ARTICLE_BOARD_ORDER, TARGET_URL):
     except:
         return True
     jres = jres['result']
-
-    FIRST_ARTICLE_TITLE = jres['newsList'][0]['tit']
-    print('FIRST_ARTICLE_TITLE:', FIRST_ARTICLE_TITLE)
-    
-    
-    # 데이터베이스 연결 초기화
-    db.open_connect()
-
-    # 연속키 데이터베이스화 작업
-    # 연속키 데이터 저장 여부 확인 구간
-    dbResult = db.SelNxtKey(SEC_FIRM_ORDER, ARTICLE_BOARD_ORDER)
-    if dbResult: # 연속키가 존재하는 경우
-        print('데이터베이스에 연속키가 존재합니다. ', '(네이버 뉴스)')
-    else: # 연속키가 존재하지 않는 경우 => 첫번째 게시물 연속키 정보 데이터 베이스 저장
-        print('데이터베이스에 ', '(네이버 뉴스)')
-        NXT_KEY = db.InsNxtKey(SEC_FIRM_ORDER, ARTICLE_BOARD_ORDER, FIRST_ARTICLE_TITLE)
-
 
     
     category = 'flashnews'
@@ -321,8 +239,6 @@ def remove_duplicates(a_data, b_data):
     return result
 
 def NAVERNews_parse_1(ARTICLE_BOARD_ORDER, TARGET_URL):
-    global NXT_KEY
-    global TEST_SEND_YN
 
     logging.debug('NAVERNews_parse_1')
     request = urllib.request.Request(TARGET_URL, headers={'User-Agent': 'Mozilla/5.0'})
@@ -447,49 +363,6 @@ async def sendMessage(sendMessageText): #실행시킬 함수명 임의지정
     bot = telegram.Bot(token = SECRET_KEY.TELEGRAM_BOT_TOKEN_REPORT_ALARM_SECRET)
     return await bot.sendMessage(chat_id = GetSendChatId(), text = sendMessageText, disable_web_page_preview = True, parse_mode = "Markdown")
 
-async def sendDocument(ATTACH_FILE_NAME): #실행시킬 함수명 임의지정
-    global CHAT_ID
-    bot = telegram.Bot(token = SECRET_KEY.TELEGRAM_BOT_TOKEN_REPORT_ALARM_SECRET)
-    return await bot.sendDocument(chat_id = GetSendChatId(), document = open(ATTACH_FILE_NAME, 'rb'))
-
-# 최초 send함수
-# URL(프리뷰해제) 발송 + 해당 레포트 pdf 발송
-def send(ARTICLE_BOARD_NAME , ARTICLE_TITLE , ARTICLE_URL): # 파일의 경우 전역변수로 처리 (downloadFile 함수)
-    global CHAT_ID
-
-    print('send()')
-    DISABLE_WEB_PAGE_PREVIEW = True # 메시지 프리뷰 여부 기본값 설정
-
-    # 실제 전송할 메시지 작성
-    sendMessageText = ''
-    sendMessageText += GetSendMessageTitle()
-    sendMessageText += ARTICLE_TITLE + "\n"
-    sendMessageText += EMOJI_PICK + ARTICLE_URL 
-
-    #생성한 텔레그램 봇 정보(@ebest_noti_bot)
-    bot = telegram.Bot(token = SECRET_KEY.TELEGRAM_BOT_TOKEN_REPORT_ALARM_SECRET)
-
-    #생성한 텔레그램 봇 정보 출력
-    #me = bot.getMe()
-    #print('텔레그램 채널 정보 :',me)
-
-    if SEC_FIRM_ORDER == 999 or SEC_FIRM_ORDER == 998 or SEC_FIRM_ORDER == 997 : # 매매동향의 경우 URL만 발송하여 프리뷰 처리 
-        DISABLE_WEB_PAGE_PREVIEW = False
-
-    #bot.sendMessage(chat_id = GetSendChatId(), text = sendMessageText, disable_web_page_preview = DISABLE_WEB_PAGE_PREVIEW)
-    asyncio.run(sendMessage(sendMessageText)) #봇 실행하는 코드
-
-    if DISABLE_WEB_PAGE_PREVIEW: # 첨부파일이 있는 경우 => 프리뷰는 사용하지 않음
-        try:
-            time.sleep(1) # 메시지 전송 텀을 두어 푸시를 겹치지 않게 함
-            #bot.sendDocument(chat_id = GetSendChatId(), document = open(ATTACH_FILE_NAME, 'rb'))
-            asyncio.run(sendDocument(ATTACH_FILE_NAME)) #봇 실행하는 코드
-            os.remove(ATTACH_FILE_NAME) # 파일 전송 후 PDF 삭제
-        except:
-            return
-    
-    time.sleep(1) # 모바일 알림을 받기 위해 8초 텀을 둠(loop 호출시)
-
 # 가공없이 텍스트를 발송합니다.
 def sendText(sendMessageText): 
     global CHAT_ID
@@ -520,42 +393,6 @@ def sendAddText(sendMessageText, sendType='N'):
 
     return ''
 
-
-# URL에 파일명을 사용할때 한글이 포함된 경우 인코딩처리 로직 추가 
-def DownloadFile(URL, FILE_NAME):
-    global ATTACH_FILE_NAME
-    print("DownloadFile()")
-
-    if SEC_FIRM_ORDER == 6: # 교보증권 예외 로직
-        # 로직 사유 : 레포트 첨부파일명에 한글이 포함된 경우 URL처리가 되어 있지 않음
-        CONVERT_URL = URL 
-        for c in URL: # URL내 한글이 있는 경우 인코딩 처리(URL에 파일명을 이용하여 조합함)
-            # 코드셋 기준 파이썬:UTF-8 . 교보증권:EUC-KR
-            # 1. 주소에서 한글 문자를 판별
-            # 2. 해당 문자를 EUC-KR로 변환후 URL 인코딩
-            print("##",c , "##", ord('가') <= ord(c) <= ord('힣') )
-            if ord('가') <= ord(c) <= ord('힣'): 
-                c_encode = c.encode('euc-kr')
-                CONVERT_URL = CONVERT_URL.replace(c, urlparse.quote(c_encode) )
-
-        if URL != CONVERT_URL: 
-            print("기존 URL에 한글이 포함되어 있어 인코딩처리함")
-            print("CONVERT_URL", CONVERT_URL)
-            URL = CONVERT_URL
-
-    ATTACH_FILE_NAME = re.sub('[\/:*?"<>|]','',FILE_NAME) # 저장할 파일명 : 파일명으로 사용할수 없는 문자 삭제 변환
-    print('convert URL:',URL)
-    print('convert ATTACH_FILE_NAME:',ATTACH_FILE_NAME)
-    with open(ATTACH_FILE_NAME, "wb")as file:  # open in binary mode
-        response = get(URL, verify=False)     # get request
-        file.write(response.content) # write to file
-        
-     
-    r = googledrive.upload(str(ATTACH_FILE_NAME))
-    print('********************')
-    print(f'main URL {r}')
-    return r
-
 def GetSendMessageText(INDEX, ARTICLE_BOARD_NAME , ARTICLE_TITLE , ARTICLE_URL):
     # 실제 전송할 메시지 작성
     sendMessageText = ''
@@ -576,24 +413,11 @@ def GetSendMessageTitle():
     SendMessageTitle = ""
     msgFirmName = ""
     
-    if SEC_FIRM_ORDER == 999:
-        msgFirmName = "매매동향"
-    elif SEC_FIRM_ORDER == 998:
+    if SEC_FIRM_ORDER == 998:
         msgFirmName = "네이버 - "
         if  ARTICLE_BOARD_ORDER == 0 : msgFirmName += "실시간 뉴스 속보"
         else: msgFirmName += "가장 많이 본 뉴스"
-    elif SEC_FIRM_ORDER == 997: msgFirmName = "아이투자 - 랭킹스탁"
-    elif SEC_FIRM_ORDER == 996: msgFirmName = "연합인포맥스 - 공매도 잔고 상위"
     elif SEC_FIRM_ORDER == 995: msgFirmName = "조선비즈 - C-Biz봇"
-    elif SEC_FIRM_ORDER == 994: msgFirmName = "매경 증권 52주 신고저가 알림"
-    elif SEC_FIRM_ORDER == 900: 
-        msgFirmName = "[네이버 증권 "
-        if ARTICLE_BOARD_ORDER == 0 : msgFirmName += "기업 리서치](https://m.stock.naver.com/investment/research/company)"
-        elif ARTICLE_BOARD_ORDER == 1:  msgFirmName += "산업 리서치](https://m.stock.naver.com/investment/research/industry)"
-        else: print(msgFirmName)
-    elif SEC_FIRM_ORDER == 123: msgFirmName = "[오늘의 레포트](https://comp.fnguide.com/SVO/WooriRenewal/Report.asp)"
-    else: # 증권사
-        msgFirmName =  GetFirmName() 
 
     # SendMessageTitle += "\n" + EMOJI_FIRE + msgFirmName + EMOJI_FIRE + "\n" 
     SendMessageTitle += "\n\n" + " ●"+  msgFirmName + "\n" 
@@ -607,23 +431,14 @@ def GetSendChatId():
             SendMessageChatId = SECRET_KEY.TELEGRAM_CHANNEL_ID_NAVER_FLASHNEWS # 네이버 실시간 속보 뉴스 채널
         else:
             SendMessageChatId = SECRET_KEY.TELEGRAM_CHANNEL_ID_NAVER_RANKNEWS # 네이버 많이본 뉴스 채널
-    elif SEC_FIRM_ORDER == 997:
-            SendMessageChatId = SECRET_KEY.TELEGRAM_CHANNEL_ID_ITOOZA # 아이투자
     elif SEC_FIRM_ORDER == 995:
             SendMessageChatId = SECRET_KEY.TELEGRAM_CHANNEL_ID_CHOSUNBIZBOT # 조선비즈 C-bot
-    elif SEC_FIRM_ORDER == 123: # 오늘의 레포트 채널 나누기 
-        SendMessageChatId = SECRET_KEY.TELEGRAM_CHANNEL_ID_TODAY_REPORT # 오늘의 레포트 채널
-    else:
-        SendMessageChatId = SECRET_KEY.TELEGRAM_CHANNEL_ID_REPORT_ALARM # 운영 채널(증권사 신규 레포트 게시물 알림방)
     
     # SendMessageChatId = SECRET_KEY.TELEGRAM_CHANNEL_ID_TEST
     return SendMessageChatId
  
 def main():
     global SEC_FIRM_ORDER  # 증권사 순번
-    global REFRESH_TIME # 새로고침 주기
-    global INTERVAL_TIME # 새로고침 주기 - 파일
-    global TEST_SEND_YN
 
     # 사용자의 홈 디렉토리 가져오기
     HOME_PATH = os.path.expanduser("~")
