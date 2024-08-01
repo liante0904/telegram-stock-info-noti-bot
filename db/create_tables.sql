34de2b5b0522f70ffd731b3ff7d06a591775d00d
seunghoon shin <liante0904@gmail.com>
2024-08-01 08:38:43 +0000

    Sync changed files from master to test

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,6 +1,6 @@
 -- SQLite 데이터베이스 파일을 생성합니다.
 -- sudo apt-get install sqlite3
---sqlite3 ~/sqlite3/telegram.db < create_tables.sql
+--mkdir ~/sqlite3; sqlite3 ~/sqlite3/telegram.db < create_tables.sql
 CREATE TABLE IF NOT EXISTS hankyungconsen (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     SEC_FIRM_ORDER INTEGER,
