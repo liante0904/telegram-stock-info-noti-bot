12339c79d20190156a5d1d5cef54039373debe19
seunghoon shin <liante0904@gmail.com>
2024-09-23 15:02:33 +0900

    add daily update sql. TODO download sql & logic.

--- scrap_send_main.py
+++ scrap_send_main.py
@@ -1,13 +1,22 @@
-from package.json_to_sqlite import daily_select_data
+from package.json_to_sqlite import daily_select_data, daily_update_data
 from package.sqlite_util import convert_sql_to_telegram_messages
 
 def main():
-    rows = daily_select_data()
+    rows = daily_select_data(type='send')
     formatted_messages = convert_sql_to_telegram_messages(rows)
     print('='*30)
 
+    # TODO SEND
     for message in formatted_messages:
         print(message)  # 텔레그램 발송 함수
+    
+    # TOTO UPDATE
+    r = daily_update_data(rows)
+    
+    if r: print('성공')
+    
+    
+    rows = daily_select_data(type='download')
     
 
 if __name__ == "__main__":
