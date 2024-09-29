a94a0c8385c4f3d2f82f384c2c9ddf5dc7f0e576
seunghoon shin <liante0904@gmail.com>
2024-09-29 13:05:23 +0000

    Sync changed files from master to dev

--- scrap_send_main.py
+++ scrap_send_main.py
@@ -1,29 +1,56 @@
+import asyncio
 from package.json_to_sqlite import daily_select_data, daily_update_data
 from package.sqlite_util import convert_sql_to_telegram_messages
+from package.telegram_util import sendMessage
+
+def daily_report(report_type):
+    """
+    매일 보고서를 처리하는 함수입니다.
+
+    이 함수는 두 가지 타입의 작업을 지원합니다:
+    
+    - 'send': 
+        텔레그램 메시지를 전송합니다. 데이터베이스에서 데이터를 선택하고 
+        형식화하여 메시지를 생성한 후, 각 메시지를 전송합니다. 
+        이후 데이터베이스의 데이터를 업데이트합니다.
+
+    - 'download': 
+        다운로드 작업을 수행합니다. 데이터베이스에서 다운로드할 데이터를 선택하고, 
+        선택된 데이터에 대한 처리를 진행합니다. 
+        이후 데이터베이스의 데이터를 업데이트합니다.
+
+    매개변수:
+    ----------
+    report_type : str
+        'send' 또는 'download' 중 하나를 지정합니다.
+    """
+    if report_type == 'send':
+        rows = daily_select_data(type=report_type)
+        formatted_messages = convert_sql_to_telegram_messages(rows)
+        print('=' * 30)
+
+        # 메시지 발송
+        for message in formatted_messages:
+            print(f"메시지 발송 중: {message}")
+            asyncio.run(sendMessage(message))
+        # 데이터 업데이트
+        r = daily_update_data(fetched_rows=rows, type=report_type)
+        if r: 
+            print('성공')
+
+    elif report_type == 'download':
+        rows = daily_select_data(type='download')
+        print(rows)
+
+        # 파일 다운로드 처리
+        if rows:
+            r = daily_update_data(fetched_rows=rows, type='download')
+            if r: 
+                print('성공')
 
 def main():
-    rows = daily_select_data(type='send')
-    formatted_messages = convert_sql_to_telegram_messages(rows)
-    print('='*30)
-
-    # TODO SEND
-    for message in formatted_messages:
-        print(message)  # 텔레그램 발송 함수
-    
-    # TOTO UPDATE
-    r = daily_update_data(fetched_rows=rows, type='send')
-    
-    if r: print('성공')
-    
-    
-    rows = daily_select_data(type='download')
-    print(rows)
-    # TOTO FILE DOWNLOAD PROCESS
-    if rows:
-        # TOTO UPDATE
-        r = daily_update_data(fetched_rows=rows, type='download')
-        if r: print('성공')
-        
-
+    # 발송될 내역
+    daily_report(report_type='send')
+    daily_report(report_type='download')
 if __name__ == "__main__":
     main()
