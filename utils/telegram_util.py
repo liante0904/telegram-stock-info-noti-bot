aa4173b3b1efa0e828dd4e69e0ccf3efc7ae6aee
Seunghoon Shin <liante0904@gmail.com>
2024-10-09 19:33:41 +0900

    clean up code.

--- utils/telegram_util.py
+++ utils/telegram_util.py
@@ -15,12 +15,6 @@ else:
     TELEGRAM_BOT_TOKEN_REPORT_ALARM_SECRET = os.getenv('TELEGRAM_BOT_TOKEN_REPORT_ALARM_SECRET')
     TELEGRAM_CHANNEL_ID_REPORT_ALARM = os.getenv('TELEGRAM_CHANNEL_ID_REPORT_ALARM')
 
-
-async def sendMessage(sendMessageText): #실행시킬 함수명 임의지정
-    bot = telegram.Bot(token = TELEGRAM_BOT_TOKEN_REPORT_ALARM_SECRET)
-    return await bot.sendMessage(chat_id = TELEGRAM_CHANNEL_ID_REPORT_ALARM, text = sendMessageText, disable_web_page_preview = True, parse_mode = "Markdown")
-
-
 # 가공없이 텍스트를 발송합니다.
 async def sendMarkDownText(token, chat_id, sendMessageText): 
     time.sleep(1)
