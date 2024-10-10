c95e4a253d625ecc2b7e300183ca8caa6bf62cc1
seunghoon shin <liante0904@gmail.com>
2024-10-10 16:09:17 +0900

    add Leading

--- scrap_main.py
+++ scrap_main.py
@@ -1636,6 +1636,73 @@ def TOSSinvest_checkNewArticle():
 
     return nNewArticleCnt
 
+def Leading_checkNewArticle():
+    SEC_FIRM_ORDER = 16
+    ARTICLE_BOARD_ORDER = 0
+
+    requests.packages.urllib3.disable_warnings()
+
+    # 리딩투자증권 
+    TARGET_URL_0 =  "http://www.leading.co.kr/board/EquityResearch/list"
+    
+    TARGET_URL_TUPLE = (TARGET_URL_0, )#TARGET_URL_1, TARGET_URL_2, TARGET_URL_3, TARGET_URL_4, TARGET_URL_5, TARGET_URL_6, TARGET_URL_7, TARGET_URL_8)
+
+    
+    # URL GET
+    for ARTICLE_BOARD_ORDER, TARGET_URL in enumerate(TARGET_URL_TUPLE):
+        firm_info = FirmInfo(
+            sec_firm_order=SEC_FIRM_ORDER,
+            article_board_order=ARTICLE_BOARD_ORDER
+        )
+        
+        scraper = ''
+        try:
+            scraper = WebScraper(TARGET_URL, firm_info)
+            # response.raise_for_status()  # 오류가 발생하면 예외를 발생시킵니다.
+        except requests.exceptions.RequestException as e:
+            print("웹 페이지에 접속하는 중 오류가 발생했습니다:", e)
+        
+        soupList = scraper.Get()
+        print('='*40)
+        # print(soupList[0])
+        
+        nNewArticleCnt = 0
+        # soupList에서 게시물 정보 파싱
+        for list in soupList:
+            # 제목 파싱
+            title_element = list.select_one("td.align-left a")  # 제목이 들어 있는 a 태그 선택
+            if not title_element:  # 제목 요소가 없는 경우
+                continue  # 건너뜁니다.
+            title = title_element.get_text(strip=True)  # 제목 텍스트 추출 (공백 제거)
+            
+            # 리서치 파일 첨부 링크 파싱
+            attachment_element = list.select_one("td:nth-child(4) > a")  # 'file-icon' 클래스를 가진 a 태그 선택 (리서치 File)
+            attachment_link = "없음"
+            if attachment_element and 'href' in attachment_element.attrs:
+                attachment_link =  f"http://www.leading.co.kr/download?{attachment_element['href']}"  # 상대 경로를 절대 경로로 변환
+            
+            # 결과 출력
+            print("제목:", title)
+            print("첨부 파일:", attachment_link)
+            print()
+            LIST_ARTICLE_TITLE = title
+            LIST_ARTICLE_URL = attachment_link
+            if save_data_to_local_json(
+                filename='./json/data_main_daily_send.json',
+                sec_firm_order=SEC_FIRM_ORDER,
+                article_board_order=ARTICLE_BOARD_ORDER,
+                firm_nm=firm_info.get_firm_name(),
+                attach_url=LIST_ARTICLE_URL,
+                article_title=LIST_ARTICLE_TITLE): nNewArticleCnt += 1 # 새로운 게시글 수
+            
+
+
+    # 메모리 정리
+    del soupList, list
+    gc.collect()
+
+    return nNewArticleCnt
+
 def main():
     # 사용자의 홈 디렉토리 가져오기
     HOME_PATH = os.path.expanduser("~")
@@ -1686,6 +1753,7 @@ def main():
         Koreainvestment_selenium_checkNewArticle,
         DAOL_checkNewArticle,
         # TOSSinvest_checkNewArticle,
+        # Leading_checkNewArticle,
     ]
 
     for check_function in check_functions:
