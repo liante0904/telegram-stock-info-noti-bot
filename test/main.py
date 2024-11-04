ee6cd5478b859ed5a6e7485d0f559cfe25b4f8d3
seunghoon shin <liante0904@gmail.com>
2024-11-04 14:28:14 +0900

    add DS
    block dbfi

---
+++ test/main.py
@@ -0,0 +1,26 @@
+import asyncio
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from models.SQLiteManager import SQLiteManager
+from models.FirmInfo import FirmInfo
+from modules.dbfi_19 import dbfi_checkNewArticle
+from package.json_to_sqlite import insert_json_data_list
+
+async def main():
+    r = await dbfi_checkNewArticle()
+    insert_json_data_list(r, 'data_main_daily_send')
+    firm_info = FirmInfo(
+        sec_firm_order=19,
+        article_board_order=0
+    )
+    
+    db = SQLiteManager()
+    # r = db.fetch_all(table_name='data_main_daily_send')
+    r = await db.fetch_daily_articles_by_date(firm_info=firm_info,date_str='20241104')
+    print(r)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
