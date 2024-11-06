05a21e3b7af9fe38d9c46bf2e0a08feb70934967
Seunghoon Shin <liante0904@gmail.com>
2024-11-06 14:23:08 +0000

    Sync changed files from master to test

--- modules/LS_1.py
+++ modules/LS_1.py
@@ -20,6 +20,16 @@ def LS_detail(articles, firm_info):
         
         # HTML parse
         soup = scraper.Get()
+        img = soup.select_one('#contents > div.tbViewCon > div > html > body > p > img')
+        
+        alt_value = img.get("alt")
+
+        # alt 값에서 필요한 정보만 추출하고 순서 재배치
+        base_value = alt_value.split(".")[0]  # 확장자를 제외한 부분 추출
+        parts = base_value.split("_")  # 언더스코어를 기준으로 분할
+
+        # parts[2] (날짜) + parts[0] + parts[1] 순으로 배치하여 URL 생성
+        url = f"https://msg.ls-sec.co.kr/eum/K_{parts[2]}_{parts[0]}_{parts[1]}.pdf"
 
         # 게시글 제목
         trs = soup.select('tr')
@@ -29,33 +39,37 @@ def LS_detail(articles, firm_info):
         # 첨부파일 URL 조립 예시  
         # => https://www.ls-sec.co.kr/upload/EtwBoardData/B202410/20241002_한국 9월 소비자물가.pdf
         
-        # B포스팅 월
-        URL_PARAM = article["REG_DT"]  # 예: "20231105" 형식
-        URL_PARAM_0 = 'B' + URL_PARAM[:6]  # 'BYYYYMM' 형식으로 변환
+        # # B포스팅 월
+        # URL_PARAM = article["REG_DT"]  # 예: "20231105" 형식
+        # URL_PARAM_0 = 'B' + URL_PARAM[:6]  # 'BYYYYMM' 형식으로 변환
 
-        # 첨부 파일 이름과 URL 인코딩 처리
-        ATTACH_FILE_NAME = soup.select_one('.attach > a').get_text()
-        ATTACH_URL_FILE_NAME = ATTACH_FILE_NAME.replace(' ', "%20").replace('[', '%5B').replace(']', '%5D').replace('%25', '%') 
-        URL_PARAM_1 = urllib.parse.unquote(ATTACH_URL_FILE_NAME)
+        # # 첨부 파일 이름과 URL 인코딩 처리
+        # ATTACH_FILE_NAME = soup.select_one('.attach > a').get_text()
+        # ATTACH_URL_FILE_NAME = ATTACH_FILE_NAME.replace(' ', "%20").replace('[', '%5B').replace(']', '%5D').replace('%25', '%') 
+        # URL_PARAM_1 = urllib.parse.unquote(ATTACH_URL_FILE_NAME)
 
-        # 최종 첨부 파일 URL 생성
-        ATTACH_URL = 'https://www.ls-sec.co.kr/upload/EtwBoardData/{0}/{1}'
-        ATTACH_URL = ATTACH_URL.format(URL_PARAM_0, URL_PARAM_1)
+        # # 최종 첨부 파일 URL 생성
+        # ATTACH_URL = 'https://www.ls-sec.co.kr/upload/EtwBoardData/{0}/{1}'
+        # ATTACH_URL = ATTACH_URL.format(URL_PARAM_0, URL_PARAM_1)
         
-        # URL 인코딩 => 사파리 한글처리 
-        article['LIST_ARTICLE_URL'] = urllib.parse.quote(ATTACH_URL, safe=':/')
-        article['TELEGRAM_URL'] = urllib.parse.quote(ATTACH_URL, safe=':/')
+        # # URL 인코딩 => 사파리 한글처리 
+        # article['LIST_ARTICLE_URL'] = urllib.parse.quote(ATTACH_URL, safe=':/')
+        # article['TELEGRAM_URL'] = urllib.parse.quote(ATTACH_URL, safe=':/')
+
+        article['LIST_ARTICLE_URL'] = url
+        article['TELEGRAM_URL'] = url
         
         # item['LIST_ARTICLE_TITLE'] = LIST_ARTICLE_TITLE
         # print(article)
         # print('*********확인용**************')
     print(articles)
+    
     return articles
         
 
 
 if __name__ == "__main__":
-    test = [{"KEY": "https://www.ls-sec.co.kr/EtwFrontBoard/View.jsp?skey=&sval=&board_no=36&category_no=&left_menu_no=&front_menu_no=&sub_menu_no=&parent_menu_no=&currPage=1&board_seq=7679835",
+    test = [{"KEY": "https://www.ls-sec.co.kr/EtwFrontBoard/View.jsp?skey=&sval=&board_no=36&category_no=&left_menu_no=&front_menu_no=&sub_menu_no=&parent_menu_no=&currPage=1&board_seq=7679855",
              "REG_DT": "20241106"
              }]
     
