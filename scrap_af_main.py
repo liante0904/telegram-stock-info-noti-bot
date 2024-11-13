9acc19b31881439abb2c27e364a8002228026e30
Seunghoon Shin <liante0904@gmail.com>
2024-11-13 16:48:20 +0000

    Sync changed files from master to test

--- scrap_af_main.py
+++ scrap_af_main.py
@@ -43,11 +43,16 @@ async def update_firm_telegram_url_by_date(date_str=None):
                 elif sec_firm_order == 0:
                     # sec_firm_order가 0인 경우 추가 작업 수행
                     print("Additional processing for SEC_FIRM_ORDER 0")
-                    update_records = LS_detail(articles=records, firm_info=firm_info)  # all_records 대신 records 사용
-                    
-                    for record in update_records:
+                    print(f"records{len(records)}")
+                    for record in records:
+                        LS_detail(articles=record, firm_info=firm_info)  # all_records 대신 records 사용
                         await db.update_telegram_url(record['id'], record['TELEGRAM_URL'], record['ARTICLE_TITLE'])
-                        print(f"Updated TELEGRAM_URL for id {record['id']} with {record['TELEGRAM_URL']}")
+
+                    # update_records = LS_detail(articles=records, firm_info=firm_info)  # all_records 대신 records 사용
+                    
+                    # for record in update_records:
+                    #     await db.update_telegram_url(record['id'], record['TELEGRAM_URL'], record['ARTICLE_TITLE'])
+                    #     print(f"Updated TELEGRAM_URL for id {record['id']} with {record['TELEGRAM_URL']}")
 
     # 전체 회사들의 레코드가 JSON 리스트로 모임
     if all_records:  # all_records가 비어 있지 않은 경우에만 JSON 변환 및 출력 수행
