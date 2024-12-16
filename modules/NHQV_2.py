db9ca0a3272d7373c4afec778a9b7ba265fcc6fd
Seunghoon Shin <liante0904@gmail.com>
2024-12-16 10:02:15 +0900

    fix NHQV REG_DT, KEY

--- modules/NHQV_2.py
+++ modules/NHQV_2.py
@@ -81,11 +81,11 @@ async def NHQV_checkNewArticle(target_date=None):
                     "SEC_FIRM_ORDER": SEC_FIRM_ORDER,
                     "ARTICLE_BOARD_ORDER": ARTICLE_BOARD_ORDER,
                     "FIRM_NM": firm_info.get_firm_name(),
-                    "REG_DT": article['rshPprDruDtNm'].replace("-", ""),
+                    "REG_DT": article['rshPprDruDtNm'].replace(".", ""),
                     "WRITER": article['rshPprDruEmpFnm'],
-                    "ATTACH_URL": article['hpgeFleUrlCts'],
-                    "DOWNLOAD_URL": article['hpgeFleUrlCts'],
+                    "TELEGRAM_URL": article['hpgeFleUrlCts'],
                     "ARTICLE_TITLE": article['rshPprTilCts'],
+                    "KEY":article['hpgeFleUrlCts'],
                     "SAVE_TIME": datetime.now().isoformat()
                 })
 
