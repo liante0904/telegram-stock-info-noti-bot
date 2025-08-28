d9b2bdc21297561116811fd29fd6011e1a44a3d0
Seunghoon Shin <liante0904@gmail.com>
2025-08-28 21:12:55 +0900

    비동기 처리를 위해 sendMarkDownText 함수에서 time.sleep을 await asyncio.sleep으로 변경

--- utils/telegram_util.py
+++ utils/telegram_util.py
@@ -1,8 +1,8 @@
-import time
+import asyncio
 import telegram
 
 # 가공없이 텍스트를 발송합니다.
 async def sendMarkDownText(token, chat_id, sendMessageText): 
-    time.sleep(1)
+    await asyncio.sleep(1)
     bot = telegram.Bot(token = token)
     await bot.sendMessage(chat_id = chat_id, text = sendMessageText, disable_web_page_preview = True, parse_mode = "Markdown")
